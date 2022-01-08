c18ab97b16454e860cd34541d33e064f950470ee
John Brewer <jbrewer@jera.com>
2022-01-07 19:14:35 -0800

    Initial Commit

---
+++ CVFunhouse2/CVFunhouse2.h
@@ -0,0 +1,18 @@
+//
+//  CVFunhouse2.h
+//  CVFunhouse2
+//
+//  Created by John Brewer on 1/7/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CVFunhouse2.
+FOUNDATION_EXPORT double CVFunhouse2VersionNumber;
+
+//! Project version string for CVFunhouse2.
+FOUNDATION_EXPORT const unsigned char CVFunhouse2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CVFunhouse2/PublicHeader.h>
+
+
